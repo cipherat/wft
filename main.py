a5e9c07f952a905457de4d819f9ecb6a42cee966
cipherat <cipherat@tutanota.com>
2024-10-05 01:51:28 +0300

    minor changes: import needed libraries, added comments

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
-from src import wft
+from src import wft, utils
 
-def init_file(filename):
-    with open(filename, 'w+') as fd:
-        fd.write("{}")
-    return filename
 
 if __name__ == "__main__":
     filename = "tasks.json"
-    if _fn:=input("use file (Enter for default '%s'): " % (filename)):
-        filename = init_file(_fn)
+
+    if _fn:=input("use file (Enter for default '%s'): " % (filename)): # use given input, otherwise -empty- skip
+        filename = utils.init_file(_fn)
 
     with open(filename, 'r+') as file:
         wft.run(file)
