4fc5e85bb46729ddbeddae1ca79f9d57cd4682d6
cipherat <cipherat@tutanota.com>
2024-10-04 04:38:30 +0300

    major changes: added user file choice

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-#!./venv/bin/python3
+#!/usr/bin/env python3
 from src import wft
 
+def init_file(filename):
+    with open(filename, 'w+') as fd:
+        fd.write("{}")
+    return filename
+
 if __name__ == "__main__":
-    filename = "tasks.json" # Feature (#81AB01)
+    filename = "tasks.json"
+    if _fn:=input("use file (Enter for default '%s'): " % (filename)):
+        filename = init_file(_fn)
+
     with open(filename, 'r+') as file:
         wft.run(file)
